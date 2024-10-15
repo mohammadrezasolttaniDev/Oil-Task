b8fffa90c2999f0ec9c45981ad76113864682117
Admin <mohammadrezasolttani@gmail.com>
2024-10-15 18:09:32 +0330

    #7 create Services (Service , User)

---
+++ src/main/java/org/example/Service/UserService.java
@@ -0,0 +1,9 @@
+package org.example.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+}
